aaecf18cfb6ff77f579b8e9c18b9440261fb3595
arumugamrethinam23 <arumugamrethinam23@gmail.com>
2025-11-30 14:06:45 +0530

    Update MethodsOverloading.java

--- src/com/basics/MethodsOverloading.java
+++ src/com/basics/MethodsOverloading.java
@@ -3,7 +3,7 @@ package com.basics;
 public class MethodsOverloading {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			System.out.println("welcome home");
 
 	}
 
