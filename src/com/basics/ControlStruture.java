b2e7a2b4d52f87c361f775d1c9024963ea404d14
arumugamrethinam23 <arumugamrethinam23@gmail.com>
2025-12-01 12:44:49 +0530

    first commit

--- src/com/basics/ControlStruture.java
+++ src/com/basics/ControlStruture.java
@@ -5,7 +5,7 @@ public class ControlStruture {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int age = 65;
-
+    //For loop 
         if (age < 0) {
             System.out.println("Invalid age!");
         } 
@@ -21,7 +21,19 @@ public class ControlStruture {
         else {
             System.out.println("You are a Senior Citizen.");
         }
-  
+        //While loop stops when condition is met
+        
+        System.out.println("Stop when we find number 8");
+
+        int value = 1;
+        while (value <= 10) {
+            if (value == 8) {
+                System.out.println("Found 8! Stopping loop.");
+                break; // exits the loop
+            }
+            System.out.println("Checking value: " + value);
+            value++;
+        }
 	}
 
 }
